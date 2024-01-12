9f380b436cb4ac36bb299f0c145ad68f2adddf30
Olga Panfilova <gr-olga@ya.ru>
2024-01-12 10:25:10 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <h1>Client Form</h1>
+    <div>
+      <client-form @form-submitted="handleFormSubmission" />
+    </div>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+import ClientForm from '@/components/ClientForm.vue';
+
+export default defineComponent({
+  components: {
+    ClientForm,
+  },
+  methods: {
+    handleFormSubmission(formData) {
+      // You can handle the form submission logic here
+      console.log('Form submitted with data:', formData);
+    },
+  },
+});
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/ClientForm.vue
@@ -0,0 +1,70 @@
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  data() {
+    return {
+      formData: {
+        firstName: '',
+        lastName: '',
+        dateOfBirth: '',
+        phoneNumber: '',
+        email: '',
+        bankAccountNumber: '',
+      },
+    };
+  },
+  methods: {
+    submitForm() {
+      this.$emit('form-submitted', this.formData);
+    },
+  },
+});
+</script>
+
+<template>
+  <div >
+    <form @submit.prevent="submitForm">
+      <div>
+        <label for="firstname">First Name:</label>
+        <input type="text" id="firstname" v-model="formData.firstName" required>
+      </div>
+
+      <div>
+        <label for="lastname">Last Name:</label>
+        <input type="text" id="lastname" v-model="formData.lastName" required>
+      </div>
+
+      <div>
+        <label for="dob">Date of Birth:</label>
+        <input type="date" id="dob" v-model="formData.dateOfBirth" required>
+      </div>
+
+      <div>
+        <label for="phone">Phone Number:</label>
+        <input type="tel" id="phone" v-model="formData.phoneNumber" required>
+      </div>
+
+      <div>
+        <label for="email">Email:</label>
+        <input type="email" id="email" v-model="formData.email" required>
+      </div>
+
+      <div>
+        <label for="bank">Bank Account Number:</label>
+        <input type="text" id="bank" v-model="formData.bankAccountNumber" required>
+      </div>
+
+      <div>
+        <button type="submit">Submit</button>
+      </div>
+    </form>
+  </div>
+</template>
+
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
